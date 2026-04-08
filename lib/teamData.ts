1d28cd604d613d82e7438a1465e4dc5a6788f9c6
Pablo Rahonne <pablorahonne.pr@gmail.com>
2026-04-08 19:58:27 -0300

    feat: Add player image to England team data
    Includes the image URL for Harry Kane, enhancing the detail of player information.

--- lib/teamData.ts
+++ lib/teamData.ts
@@ -4323,7 +4323,7 @@ A Inglaterra chega ao Mundial de 2026 com a missão de finalmente "trazer o fute
       image: 'https://img.sofascore.com/api/v1/manager/785186/image'
     },
     players: [
-      { name: 'Harry Kane', position: 'Atacante', club: 'Bayern Munich', highlight: 'Capitão e maior artilheiro da história da seleção, referência absoluta no ataque.' },
+      { name: 'Harry Kane', position: 'Atacante', club: 'Bayern Munich', highlight: 'Capitão e maior artilheiro da história da seleção, referência absoluta no ataque.', image:'https://img.sofascore.com/api/v1/player/108579/image' },
       { name: 'Jude Bellingham', position: 'Meia', club: 'Real Madrid', highlight: 'Um dos melhores jogadores do mundo, motor do meio-campo e decisivo no ataque.' },
       { name: 'Phil Foden', position: 'Meia', club: 'Manchester City', highlight: 'Talento puro e criatividade, capaz de desequilibrar qualquer defesa com sua técnica.' }
     ],
